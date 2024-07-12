eb6b6084bf04bd6872ec12da925011ec061a5646
Igor Storchevoy <hellsgor@gmail.com>
2024-07-13 01:32:32 +0400

    Update: displayed  FetchPostListView

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import { FetchPostListView } from './components/PostListView';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <FetchPostListView />
+    </div>
+  );
 }
 
 export default App;
