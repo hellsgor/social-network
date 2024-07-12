992683cbe29571ecd82ada403464abac1f9fea97
Igor Storchevoy <hellsgor@gmail.com>
2024-07-13 01:05:11 +0400

    Add: added usePostList hook

--- client/src/api/Post.ts
+++ client/src/api/Post.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { z } from 'zod';
 
 export const PostSchema = z.object({
@@ -20,4 +21,56 @@ export function fetchPostList(): Promise<FetchPostListResponse> {
   return fetch('/api/posts')
     .then((response) => response.json)
     .then((data) => FetchPostListSchema.parse(data));
+}
+
+interface IdleRequestState {
+  status: 'idle';
+}
+
+interface LoadingRequestState {
+  status: 'pending';
+}
+
+interface SuccessRequestState {
+  status: 'success';
+  data: PostList;
+}
+
+interface ErrorRequestState {
+  status: 'error';
+  error: unknown;
+}
+
+type RequestState = IdleRequestState | LoadingRequestState | SuccessRequestState | ErrorRequestState;
+
+export function usePostList() {
+  const [state, setState] = useState<RequestState>({ status: 'idle' });
+
+  useEffect(() => {
+    if (state.status === 'pending') {
+      fetchPostList()
+        .then((data) => {
+          setState({ status: 'success', data: data.list });
+        })
+        .catch((error) => {
+          setState({
+            status: 'error',
+            error: error,
+          });
+        });
+    }
+  }, [state]);
+
+  useEffect(() => {
+    setState({ status: 'pending' });
+  }, []);
+
+  const refetch = () => {
+    setState({ status: 'pending' });
+  };
+
+  return {
+    state,
+    refetch,
+  };
 }
