80fdacfa5ebd18fb82f8aec153f831ab66eaf390
Igor Storchevoy <hellsgor@gmail.com>
2024-07-13 00:40:18 +0400

    Add: added fetchPostList function

--- client/src/api/Post.ts
+++ client/src/api/Post.ts
@@ -6,9 +6,18 @@ export const PostSchema = z.object({
   authorId: z.string(),
   createdAt: z.number(),
 });
-
-export const PostListSchema = z.array(PostSchema);
-
 export type Post = z.infer<typeof PostSchema>;
 
+export const PostListSchema = z.array(PostSchema);
 export type PostList = z.infer<typeof PostListSchema>;
+
+export const FetchPostListSchema = z.object({
+  list: PostListSchema,
+});
+export type FetchPostListResponse = z.infer<typeof FetchPostListSchema>;
+
+export function fetchPostList(): Promise<FetchPostListResponse> {
+  return fetch('/api/posts')
+    .then((response) => response.json)
+    .then((data) => FetchPostListSchema.parse(data));
+}
