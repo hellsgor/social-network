b52a543bdd1699e1d405d253ba8fe2fa7db2428e
Igor Storchevoy <hellsgor@gmail.com>
2024-07-13 22:36:09 +0400

    Update: added sending login data

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,12 +1,22 @@
 import { FC, FormEventHandler, useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
 
 import { FormField } from '../FormField';
 import { Button } from '../Button';
 import './LoginForm.css';
+import { login } from '../../api/User';
+import { queryClient } from '../../api/queryClient';
 
 export const LoginForm: FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const loginMutation = useMutation(
+    {
+      mutationFn: () => login(username, password),
+    },
+    queryClient,
+  );
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
@@ -22,7 +32,9 @@ export const LoginForm: FC = () => {
         <input type="password" name="password" onChange={(event) => setPassword(event.target.value)} value={password} />
       </FormField>
 
-      <Button type="submit" title="Войти" />
+      {loginMutation.error && <span>{loginMutation.error.message}</span>}
+
+      <Button type="submit" title="Войти" isLoading={loginMutation.isPending} />
     </form>
   );
 };
