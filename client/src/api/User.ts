f5bb8c29755cd65e393221cd09909b737929dba6
Igor Storchevoy <hellsgor@gmail.com>
2024-07-13 22:46:23 +0400

    Update: moved validateResponse to separate file

--- client/src/api/User.ts
+++ client/src/api/User.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { validateResponse } from './validateResponse';
 
 export const UserSchema = z.object({
   id: z.string(),
@@ -12,20 +13,15 @@ export function fetchUser(id: string): Promise<User> {
     .then((data) => UserSchema.parse(data));
 }
 
-export function registerUser(username: string, password: string): Promise<void> {
+export function registerUser(
+  username: string,
+  password: string,
+): Promise<void> {
   return fetch('api/register', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
   }).then(() => undefined);
-}
-
-async function validateResponse(response: Response): Promise<Response> {
-  if (response.ok) {
-    throw new Error(await response.text());
-  }
-
-  return response;
 }
 
 export function login(username: string, password: string): Promise<void> {
