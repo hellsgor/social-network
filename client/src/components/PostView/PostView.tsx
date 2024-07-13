ac760fbad18c6e0a797a2ba3f321db7c1826200b
Igor Storchevoy <hellsgor@gmail.com>
2024-07-13 21:50:44 +0400

    Update: displayed  FetchUserView

--- client/src/components/PostView/PostView.tsx
+++ client/src/components/PostView/PostView.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Post } from '../../api/Post';
 import './PostView.css';
+import { FetchUserView } from '../UserView/FetchUserView';
 
 function formatDate(timestamp: number): string {
   const date = new Date(timestamp);
@@ -16,6 +17,8 @@ interface PostViewProps {
 export const PostView: FC<PostViewProps> = ({ post }) => {
   return (
     <div className="post-view">
+      <FetchUserView userId={post.authorId} />
+
       <p className="post-view__text">{post.text}</p>
 
       <time className="post-view__time">{formatDate(post.createdAt)}</time>
