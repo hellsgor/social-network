c833b7decac8246c769d1377dffa03c1f92e96df
Igor Storchevoy <hellsgor@gmail.com>
2024-07-13 22:05:39 +0400

    Add: added login function

--- client/src/api/User.ts
+++ client/src/api/User.ts
@@ -19,3 +19,11 @@ export function registerUser(username: string, password: string): Promise<void> 
     body: JSON.stringify({ username, password }),
   }).then(() => undefined);
 }
+
+export function login(username: string, password: string): Promise<void> {
+  return fetch('/api/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password }),
+  }).then(() => undefined);
+}
