542e67f6f0219afa898ece827bbd3ec67ca61325
Igor Storchevoy <hellsgor@gmail.com>
2024-07-15 22:27:07 +0400

    Update: added Account component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { Account } from './components/Account/Account';
 import { FetchPostListView } from './components/PostListView';
 
 function App() {
   return (
     <div className="app">
+      <Account />
       <FetchPostListView />
     </div>
   );
